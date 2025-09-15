04ca0d7d7a2a638e097c48862337eb85f32c47b2
Ushuaya C. <ushikid@protonmail.com>
2025-09-15 12:53:43 -0700

    add Catalina array
    added an array to check for Catalina compatibility (currently the lowest macOS version we have on USB)

--- go.sh
+++ go.sh
@@ -18,6 +18,20 @@ INTERNAL_VOLUME_NAME="Macintosh HD"
 INTERNAL_VOLUME_PATH="/Volumes/Macintosh HD"
 
 # Declare arrays for each MacOS version and compatible devices
+declare -a Catalina=("MacBookAir5,1" "MacBookAir5,2" "MacBookAir6,1" "MacBookAir6,2" "MacBookAir7,1" \
+  "MacBookAir7,2" "MacBookAir8,1" "MacBookAir8,2" "MacBookAir9,1" "MacBook8,1" \
+  "MacBook9,1" "MacBook10,1" "MacBookPro9,1" "MacBookPro9,2" "MacBookPro10,1" \
+  "MacBookPro10,2" "MacBookPro11,1" "MacBookPro11,2" "MacBookPro11,3" "MacBookPro11,4" \
+  "MacBookPro11,5" "MacBookPro12,1" "MacBookPro13,1" "MacBookPro13,2" "MacBookPro13,3" \
+  "MacBookPro14,1" "MacBookPro14,2" "MacBookPro14,3" "MacBookPro15,1" "MacBookPro15,2" \
+  "MacBookPro15,3" "MacBookPro15,4" "MacBookPro16,1" "MacBookPro16,2" "MacBookPro16,3" \
+  "MacPro6,1" "MacPro7,1" "Macmini6,1" "Macmini6,2" "Macmini7,1" \
+  "Macmini8,1" "iMac13,1" "iMac13,2" "iMac14,1" "iMac14,2" \
+  "iMac14,3" "iMac14,4" "iMac15,1" "iMac16,1" "iMac16,2" \
+  "iMac17,1" "iMac18,1" "iMac18,2" "iMac18,3" "iMac19,1" \
+  "iMac19,2" "iMac20,1" "iMac20,2" "iMacPro1,1"
+)
+
 declare -a BigSur=("MacBook10,1" "MacBook9,1" "MacBook8,1" "MacBookAir10,1" "MacBookAir9,1" \
     "MacBookAir8,2" "MacBookAir8,1" "MacBookAir7,2" "MacBookAir7,1" "MacBookAir6,2" \
     "MacBookAir6,1" "MacBookPro17,1" "MacBookPro16,4" "MacBookPro16,3" "MacBookPro16,2" \
@@ -273,11 +287,14 @@ get_install_os() {
     else printf "Monterey: ✖ "
     fi
 
-    if hasVersion "$model" "${BigSur[@]}"; then printf "Big Sur: ✔\n"
-    else printf "Big Sur: ✖\n"
+    if hasVersion "$model" "${BigSur[@]}"; then printf "Big Sur: ✔ "
+    else printf "Big Sur: ✖ "
+    fi
+
+    if hasVersion "$model" "${Catalina[@]}"; then printf "Catalina: ✔\n"
+    else printf "Catalina: ✖\n"
     fi
     echo
-
     # Determine which partition scheme we are in
 	if test -e "/Volumes/e/"; then
 		echo "Legacy partition scheme found"
