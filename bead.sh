2b19dd3226f801f45cc8b11075584c0370e4bd2a
nkerschner <natekerschner@revivn.com>
2025-09-22 15:41:35 -0700

    Update bead.sh

--- bead.sh
+++ bead.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-echo "Enter the user and IP of your BEAD Host (e.g. example@127.0.0.1): "
-read BEAD_HOST
+get_bead_host() {
+    echo "Enter the user and IP of your BEAD Host (e.g. example@127.0.0.1): "
+    read BEAD_HOST
+}
 
-if scp -o StrictHostKeyChecking=no -r $BEAD_HOST:~/Desktop/Bead\ Agent /Users/shared/; then
-    echo "successfully copied Bead Agent files"
-else
-    echo "exiting"
-    exit 1
-fi
+copy_agent_from_host() {
+    if scp -o StrictHostKeyChecking=no -r $BEAD_HOST:~/Desktop/Bead\ Agent /Users/shared/; then
+        echo "successfully copied Bead Agent files"
+    else
+        echo "exiting"
+        exit 1
+    fi
+}
 
-open -a "Bead Agent"
+open_agent() {
+    open -a "Bead Agent"
+}
 
-
-
-echo "Once finished with diagnostic testing, type y to remove the files"
-read removeFiles
-while [ "$removeFiles" != "y" ]; do
+finishing_steps() {
     echo "Once finished with diagnostic testing, type y to remove the files"
-done
-rm -rf "/Users/Shared/Bead Agent"
+    read removeFiles
+    while [ "$removeFiles" != "y" ]; do
+        echo "Once finished with diagnostic testing, type y to remove the files"
+    done
+    rm -rf "/Users/Shared/Bead Agent"
+}
 
+# Main
+get_bead_host
+copy_agent_from_host
+open_agent
+finishing_steps
